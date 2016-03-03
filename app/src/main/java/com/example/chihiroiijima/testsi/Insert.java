235d6800876ea78fc4a56d3e893572e64d0c6fe8
Chihiro <ci.2480b@gmail.com>
2016-03-03 17:22:52 +0900

    insertクラスの追加 AsyncDBにinsertに対する操作(DBにinsertのデータを保存しパスを得る)を追加

--- app/src/main/java/com/example/chihiroiijima/testsi/Insert.java
+++ app/src/main/java/com/example/chihiroiijima/testsi/Insert.java
@@ -1,9 +1,16 @@
 package com.example.chihiroiijima.testsi;
 
+import android.content.Intent;
+import android.net.Uri;
+
+import static android.support.v4.app.ActivityCompat.startActivity;
+
 /**
  * First created by Chihiro Iijima on 2016/03/03.
  *
  */
+
+
 
 //insertに対する操作
 public class Insert {
@@ -13,4 +20,17 @@ public class Insert {
     Insert(){
 
     }
+
+    //Insertのプラスミドマップを標準ブラウザから描く
+    //明示的に起動させたいアクティビティ(標準ブラウザ)を指定
+    public void drawPlasmidMap(String key, String plasmid){
+
+        String uriString = "http://www.gene-design.kit.ac.jp/prasmid_android.html?insert="+key+"&plasmid="+plasmid;
+        Uri uri = Uri.parse(uriString);
+        Intent i = new Intent(Intent.ACTION_VIEW,uri);
+        i.setClassName("com.android.browser", "com.android.browser.BrowserActivity");
+
+    }
+
+
 }
